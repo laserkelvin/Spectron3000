6fee32c42cfc367005ab2b71169c979ee333545a
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2018-12-31 08:42:16 -0500

    First push upstream

---
+++ classes.py
@@ -0,0 +1,83 @@
+
+
+import base64
+import io
+import os
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from plotly import graph_objs as go
+from lmfit.models import GaussianModel
+
+from . import utils
+
+
+@dataclass
+class Spectrum:
+    df: pd.DataFrame
+    comment: str = "Observation"
+
+    @classmethod
+    def from_upload(cls, contents, filename):
+        """
+        Method for creating a Spectrum object from the Dash upload form.
+        :param contents: data stream from Dash upload form
+        :param filename: str filename
+        :return: Spectrum object
+        """
+        content_type, content_string = contents.split(",")
+        decoded = base64.b64decode(content_string)
+        df = pd.read_csv(
+            io.StringIO(decoded.decode("utf-8"))
+        )
+        spec_obj = cls(df, os.path.basename(filename))
+        return spec_obj
+
+    def create_plot(self):
+        """
+        Create a Plotly scatter trace for visualization.
+        :return: Scatter object
+        """
+        trace = go.Scatter(
+            x=self.frequency,
+            y=self.intensity,
+            name=self.comment,
+            opacity=0.7
+        )
+        return trace
+
+    def save_table(self, filepath):
+        if hasattr(self, "table"):
+            self.table.to_csv(
+                filepath,
+                index=False,
+            )
+
+
+@dataclass
+class Catalog:
+    frequency: np.array
+    intensity: np.array
+    molecule: str
+    temperature: float = 300.0
+    density: float = 1e15
+    width: float = 5.0
+
+    def generate_spectrum(self, spec_x):
+        """
+        Generate a synthetic spectrum using the catalog data. Each frequency is represented
+        by a Gaussian line profile, with the width corresponding to the Doppler width in km/s.
+
+        :param spec_x: np.array corresponding to the frequencies to be evaluated
+        :return: np.array containing y values
+        """
+        model = GaussianModel()
+        params = model.make_params()
+        spec_y = np.zeros(len(spec_x))
+        for x, y in zip(self.frequency, self.intensity):
+            params["center"] = x
+            params["amplitude"] = (10**y) * (np.sqrt(2. * np.pi) * utils.dop2freq(self.width, x))
+            params["sigma"] = utils.dop2freq(self.width, x)
+            spec_y+=model.eval(params=params, x=spec_x)
+        return spec_y
---
+++ utils.py
@@ -0,0 +1,200 @@
+
+""" units.py
+
+    Routines for performing unit conversions and quantities
+    that are often used in spectroscopy.
+"""
+
+import numpy as np
+from scipy import constants
+
+
+kbcm = constants.value("Boltzmann constant in inverse meters per kelvin") / 100.
+avo = constants.Avogadro
+eha = constants.value("Hartree energy")
+harm = constants.value("hartree-inverse meter relationship")
+jm = constants.value("joule-inverse meter relationship")
+
+
+def kappa(A, B, C):
+    # Ray's asymmetry parameter
+    return (2*B - A - C) / (A - C)
+
+
+def rotcon2pmi(rotational_constant):
+    """ Convert rotational constants in units of MHz to
+        Inertia, in units of amu A^2.
+
+        The conversion factor is adapted from:
+        Oka & Morino, JMS (1962) 8, 9-21
+        This factor comprises h / pi^2 c.
+
+        :param rotational_constant: rotational constant in MHz
+        :return:
+    """
+    return 1 / (rotational_constant / 134.901)
+
+
+def inertial_defect(rotational_constants):
+    """ Calculates the inertial defect, given three
+        rotational constants in MHz. The ordering does not
+        matter because the values are sorted.
+
+        This value is I_c - I_a - I_b; a planar molecule
+        is strictly zero.
+    """
+    # Ensure the ordering is A,B,C
+    rotational_constants = np.sort(rotational_constants)[::-1]
+    # Convert to principal moments of inertia
+    pmi = rotcon2pmi(rotational_constants)
+    return pmi[2] - pmi[0] - pmi[1]
+
+
+def hartree2wavenumber(hartree):
+    """
+    Convert Hartrees to wavenumbers.
+    :param hartree: float
+    :return: corresponding value in 1/cm
+    """
+    return hartree * (harm / 100.)
+
+
+def kjmol2wavenumber(kj):
+    """
+    Convert kJ/mol to wavenumbers
+    :param kj: float
+    :return: corresponding value in 1/cm
+    """
+    return kj * (jm / 100.) / (avo * 1000.)
+
+
+def MHz2cm(frequency):
+    """
+    Convert MHz to wavenumbers
+    :param frequency: float
+    :return: corresponding value in 1/cm
+    """
+    return (frequency / 1000.) / (constants.c / 1e7)
+
+
+def cm2MHz(wavenumber):
+    """
+    Convert wavenumbers to MHz
+    :param wavenumber: float
+    :return: corresponding value in MHz
+    """
+    return (wavenumber * (constants.c / 1e7)) * 1000.
+
+
+def hartree2kjmol(hartree):
+    """
+    Convert Hartrees to kJ/mol.
+    :param hartree: float
+    :return: converted value in kJ/mol
+    """
+    return hartree * (eha * avo / 1000.)
+
+
+def wavenumber2kjmol(wavenumber):
+    """
+    Convert wavenumbers to kJ/mol.
+    :param wavenumber: float
+    :return: converted value in kJ/mol
+    """
+    return wavenumber / (jm / 100.) / (avo * 1000.)
+
+
+def T2wavenumber(T):
+    """
+    Convert temperature in Kelvin to wavenumbers.
+    :param T: float
+    :return: corresponding value in 1/cm
+    """
+    return T * kbcm
+
+
+def wavenumber2T(wavenumber):
+    """
+    Convert wavenumbers to Kelvin
+    :param wavenumber: float
+    :return: corresponding value in K
+    """
+    return wavenumber / kbcm
+
+
+""" 
+    Astronomy units 
+
+    Conversions and useful expressions
+"""
+
+
+def dop2freq(velocity, frequency):
+    """
+    Calculates the expected frequency in MHz based on a
+    Doppler shift in km/s and a center frequency.
+    :param velocity: float
+    :param frequency: float
+    :return: Doppler shifted frequency in MHz
+    """
+    # Frequency given in MHz, Doppler_shift given in km/s
+    # Returns the expected Doppler shift in frequency (MHz)
+    return ((velocity * 1000. * frequency) / constants.c)
+
+
+def freq2vel(frequency, offset):
+    """
+    Calculates the Doppler shift in km/s based on a center
+    frequency in MHz and n offset frequency in MHz (delta nu)
+    :param frequency: float
+    :param offset: float
+    :return: Doppler shift in km/s
+    """
+    return ((constants.c * offset) / frequency) / 1000.
+
+
+def gaussian_fwhm(sigma):
+    """
+        Calculate the full-width half maximum
+        value assuming a Gaussian function.
+
+        parameters:
+        --------------
+        sigma - float for width
+
+        returns:
+        --------------
+        fwhm - float value for full-width at half-max
+    """
+    return 2. * np.sqrt(2. * np.log(2.)) * sigma
+
+
+def gaussian_height(amplitude, sigma):
+    """
+        Calculate the height of a Gaussian distribution,
+        based on the amplitude and sigma. This value
+        corresponds to the peak height at the centroid.
+
+        parameters:
+        ----------------
+        amplitude - float
+        sigma - float
+
+        returns:
+        ----------------
+        h - float
+    """
+    h = amplitude / (np.sqrt(2. * np.pi) * sigma)
+    return h
+
+
+def gaussian_integral(amplitude, sigma):
+    """
+    Calculate the integral of a Gaussian analytically using
+    the amplitude and sigma.
+    :param amplitude: amplitude of the Gaussian
+    :param sigma: width of the Gaussian
+    :return: integrated area of the Gaussian
+    """
+    integral = amplitude * np.sqrt(2. * np.pi**2. * sigma)
+    return integral
